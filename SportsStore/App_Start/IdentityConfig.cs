300b99a159c31fd8f7d856729b45b131672998b6
Adrian <adrian.ardelean95@gmail.com>
2017-01-11 14:19:04 +0200

    Install and update packages, add IdentityConfig

---
+++ SportsStore/App_Start/IdentityConfig.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(SportsStore.IdentityConfig))]
+
+namespace SportsStore
+{
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app) { }
+    }
+}
