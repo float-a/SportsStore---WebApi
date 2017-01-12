84ea6cb71358772257007513f5b1c2279bce0d28
Adrian <adrian.ardelean95@gmail.com>
2017-01-12 09:48:17 +0200

    Creat the database context classes

---
+++ SportsStore/Infrastructure/Identity/StoreIdentityDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportsStore.Infrastructure.Identity
+{
+    public class StoreIdentityDbContext : IdentityDbContext<StoreUser>
+    {
+        public StoreIdentityDbContext() : base("SportsStoreIdentityDb")
+        {
+            Database.SetInitializer<StoreIdentityDbContext>(new StoreIdentityDbInitializer());
+        }
+
+        public static StoreIdentityDbContext Create()
+        {
+            return new StoreIdentityDbContext();
+        }
+    }
+}
