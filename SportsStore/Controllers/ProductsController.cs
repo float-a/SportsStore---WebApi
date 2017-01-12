e1b526123ab00a1bde3730335770e222bbee84b8
Adrian <adrian.ardelean95@gmail.com>
2017-01-12 15:39:23 +0200

    Add validation to Product model

--- SportsStore/Controllers/ProductsController.cs
+++ SportsStore/Controllers/ProductsController.cs
@@ -27,9 +27,17 @@ namespace SportsStore.Controllers
             Product result = Repository.Products.Where(p => p.Id == id).FirstOrDefault();
             return result == null ? (IHttpActionResult)BadRequest("No product found") : Ok(result);
         }
-        public async Task PostProduct(Product product)
+        public async Task<IHttpActionResult> PostProduct(Product product)
         {
-            await Repository.SaveProductAsync(product);
+            if (ModelState.IsValid)
+            {
+                await Repository.SaveProductAsync(product);
+                return Ok();
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
         }
 
         [Authorize(Roles="Administrators")]
